c56e8fa3b1e828ce8ee2093144e4de91234c8cd1
barabas007 <shop.ru.shop@mail.ru>
2021-10-24 14:12:58 +0300

    add image plus text

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp( MyApp());
- 
- class MyApp extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   var brightnes;
 
   @override
@@ -21,11 +21,61 @@ void main() => runApp( MyApp());
           actions: <Widget>[
             IconButton(icon: Icon(Icons.settings), onPressed: () {}),
           ],
-          ),
-          body: null,
         ),
-        
-      );
-    
+        body: _buildBody(),
+      ),
+    );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: <Widget>[
+        _headerImage(),
+        SafeArea(child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            _weatherDescription(),
+          ],
+          ),
+        ),
+        ),
+      ],
+    ),
+  );
+}
+
+Image _headerImage(){
+  return Image(
+    
+    image: NetworkImage('https://images.wallpaperscraft.ru/image/single/pejzazh_gory_art_140515_480x800.jpg'),
+    fit: BoxFit.cover,
+    height: 480,
+    width: 480,
+  
+  );
+}
+
+Column _weatherDescription() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children:<Widget> [
+    Text(
+      'Tuesday - October 24 ',
+      style: TextStyle(
+        //color: Colors.white30,
+        fontSize: 32.0,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    Divider(),
+    Text(
+      'Lkskskskssmmtmtm psppspspdkldkfmngng pslsjkfhfghfgtbn, ddddddddddddddddd. GGGGGGGGGGGGGGGGGGGGG. Gffffffffffmmmmmmmmm,cmmmmmm.',
+      style: TextStyle(color: Colors.black54),
+    )
+  ],
+  );
 }
